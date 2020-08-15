cf1a06c61fa14205b3c36f21991745838e0c6c8a
Kyle Gribble <dribbleGribble0516@gmail.com>
2020-08-14 21:10:05 -0500

    MmMmMmMmMm code

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ let endYear = parseInt($endYear.attr("id"));
 let $searchButton = $("#search");
 let $resetButton = $("reset");
 
-var querURL = `jksdjkkjkdjfkld;f${searchTerm}${numRecords}${startYear}${endYear}`;
 $searchButton.on("click", searchStuff);
 $searchTerm.on('keypress', keyPress);
 $numRecords.on('keypress', keyPress);
@@ -17,17 +16,34 @@ $startYear.on('keypress', keyPress);
 $endYear.on('keypress', keyPress);
 
 
-
-
 function keyPress(e)
 {
      if (e.key === 'Enter')
      {
-       searchStuff();
+          searchStuff();
      }
 }
-function searchStuff()
+function searchStuff(event)
 {
-     //ajax query
+     event.preventDefault();
      console.log('Script loaded properly');
+     var querURL = `jksdjkkjkdjfkld;f${searchTerm}${numRecords}${startYear}${endYear}`;
+     //ajax query here
+        var begindate = startYear;
+        var enddate = endYear;
+        var queryURL =`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&begin_date=${5.k//4797m8,ouiklju75.,6398}0101&end_date=${enddate}1231&api-key=BWMFLVuMp4tJhBEvdnmPA01G4KEIeTbp`;
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+          }).then(function(response) {
+          console.log(response);
+          for(i=0; i<response.response.docs.length; i++){
+          console.log(response.response.docs[i].abstract);
+          console.log(response.response.docs[i].web_url);
+          }
+          let $articles = $("#articles");
+          for(let i = 1; i < response.response.docs.length; i++)
+          {
+               $articles
+          })
 }
